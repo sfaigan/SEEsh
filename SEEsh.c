92bf02880eae69caf6bcd58ad7fcb04ec276e196
Shea Faigan <stfaigan@gmail.com>
2020-01-30 23:08:03 -0800

    Create SEEsh.c
    Skeleton code

---
+++ SEEsh.c
@@ -0,0 +1,20 @@
+// Configure the environment for the shell
+void initialize() {
+  // Open .SEEshrc
+  // Run commands
+}
+
+// Loop: { read and parse user input, execute relevant command }
+void interpret(int argc, char **argv) {
+  // Read input
+  // Tokenize input into command and arguments
+  // Execute command
+}
+
+int main(int argc, char **argv) {
+  initialize();
+
+  interpret(argc, argv);
+
+  return 0;
+}
